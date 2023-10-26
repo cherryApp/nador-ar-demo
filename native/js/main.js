4e115bb66d05edb9a6be5e8876c0c769f998efde
cherryApp <j.cserko@gmail.com>
2023-10-26 11:12:56 +0200

    set position

--- native/js/main.js
+++ native/js/main.js
@@ -1,6 +1,14 @@
+const REFRESH_INTERVAL = 1000;
+let lastCodeData = '';
+
 const getImage = () => {
 
   const video = document.querySelector('video#arjs-video');
+
+  if (!video) {
+    return;
+  }
+
   const canvas = document.createElement('canvas');
 
   canvas.width = video.clientWidth;
@@ -16,9 +24,54 @@ const getImage = () => {
 
   if (code && code.data) {
     const txt = `Code: ${code.data}\nName: cartoon packager`;
-    const sheet = document.querySelector('#data-sheet');
-    sheet.setAttribute('value', txt);
+    lastCodeData = txt;
   }
 };
 
-setInterval( getImage, 1000 );
+setInterval( getImage, REFRESH_INTERVAL );
+
+AFRAME.registerComponent('code-check', {
+  lastTick: 0,
+  tick: function() {
+    const currentTick = new Date().getTime();
+    if (currentTick - this.lastTick < REFRESH_INTERVAL) {
+      return;
+    }
+
+    this.lastTick = currentTick;
+
+    this.checkMarker();
+  },
+  checkMarker: function() {
+    const marker = document.querySelector('a-marker[markerhandler]');
+    if (!marker) {
+      return;
+    }
+
+    const markerPosition = new THREE.Vector3();
+    marker.object3D.getWorldPosition(markerPosition);
+
+    const sheet = document.querySelector('#data-sheet');
+    if (sheet) {
+      let text = sheet.getAttribute('text');
+      if (lastCodeData) {
+        text.value = lastCodeData;
+        sheet.setAttribute('text', text);
+      }
+
+      const sheetPosition = new THREE.Vector3();
+      sheet.object3D.getWorldPosition(sheetPosition);
+
+      // this.el.setAttribute("animation", {
+      //   property: 'position',
+      //   from: `${sheetPosition.x} ${sheetPosition.y} ${sheetPosition.z}`,
+      //   to: `${markerPosition.x} ${markerPosition.y} ${markerPosition.z}`,
+      //   dur: "1000",
+      //   easing: 'linear',
+      // })
+      sheet.setAttribute('position', markerPosition);
+      // sheet.play()
+    }
+  }
+
+});
