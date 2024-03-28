0a9f01492891af5e2b2ee1ea87b1d0369814cf06
cherryApp <j.cserko@gmail.com>
2024-03-28 14:49:08 +0100

    set map

--- ionic/src/app/page/map/map.component.ts
+++ ionic/src/app/page/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { GoogleMap, GoogleMapsModule, MapMarker } from '@angular/google-maps';
+import { IonContent, IonGrid, IonRow } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-map',
@@ -8,12 +9,32 @@ import { GoogleMapsModule } from '@angular/google-maps';
   standalone: true,
   imports: [
     GoogleMapsModule,
+    GoogleMap,
+    MapMarker,
+    IonContent,
+    IonGrid,
+    IonRow,
   ],
 })
 export class MapComponent  implements OnInit {
 
+  mapOptions = {
+    center: { lat: 47.09274595263446, lng: 17.904808187997900 },
+  };
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.addMarker();
+  }
+
+  center: google.maps.LatLngLiteral = { lat: 47.09274595263446, lng: 17.904808187997400};
+  zoom = 4;
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  addMarker() {
+    this.markerPositions.push(this.center);
+  }
 
 }
