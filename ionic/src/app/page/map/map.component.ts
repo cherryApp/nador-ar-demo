a869ffc1151972dac27dc8e07c573a0abf47b4e1
CherryApp <j.cserko@gmail.com>
2024-06-25 08:32:26 +0200

    feat: add multiple poi's

--- ionic/src/app/page/map/map.component.ts
+++ ionic/src/app/page/map/map.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap, GoogleMapsModule, MapMarker, MapAdvancedMarker, MapInfoWindow } from '@angular/google-maps';
 import { IonContent, IonGrid, IonRow } from '@ionic/angular/standalone';
 
+export interface IPoi {
+  poi: google.maps.LatLngLiteral;
+  title: string;
+  id: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -24,7 +31,14 @@ export class MapComponent implements OnInit {
 
   mapOptions = {
     center: { lat: 47.09274595263446, lng: 17.904808187997900 },
+    zoom: 7,
   };
+
+  infoTitle = '';
+
+  infoID = '';
+
+  infoImg = '';
 
   getMarker(settings: any, googleMap: any) {
     return new google.maps.Marker({
@@ -50,14 +64,36 @@ export class MapComponent implements OnInit {
     };
   }
 
+  setInfoContent(point: IPoi) {
+    this.infoTitle = point.title;
+    this.infoID = point.id;
+    this.infoImg = point.img;
+  }
+
+  pois: IPoi[] = [
+    {
+      poi: { lat: 47.09274595263446, lng: 17.904808187997400 },
+      title: 'Nádor gép',
+      id: '3344332nador',
+      img: '/assets/img/machine-001.jpg',
+    },
+    { 
+      poi: { lat: 46.89660094535991, lng: 19.66919601800715 },
+      title: 'GAMF gép',
+      id: '3344333nador',
+      img: '/assets/img/machine-002.jpg',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
     this.addMarkers();
   }
 
+
   center: google.maps.LatLngLiteral = { lat: 47.09274595263446, lng: 17.904808187997400 };
-  zoom = 4;
+  zoom = 2;
   markerOptions: google.maps.MarkerOptions = {
     draggable: false,
     icon: {
@@ -73,7 +109,8 @@ export class MapComponent implements OnInit {
     this.markerPositions.push(this.center);
   }
 
-  openInfoWindow(marker: MapMarker) {
+  openInfoWindow(marker: MapMarker, point: IPoi) {
+    this.setInfoContent(point);
     this.infoWindow.open(marker);
   }
 
